9cfd0ad9650c3c12d9332612bebf1232801f3a0c
Leonardo Andrade <154286969+LSAndradee@users.noreply.github.com>
2024-01-08 14:08:59 -0300

    Add files via upload

--- src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Pensamento } from '../pensamento';
 import { PensamentoService } from '../pensamento.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-criar-pensamento',
@@ -16,17 +17,22 @@ export class CriarPensamentoComponent implements OnInit {
     modelo: 'modelo1'
   }
 
-  constructor(private service: PensamentoService) { }
+  constructor(
+    private service: PensamentoService,
+    private router: Router
+    ) { }
 
   ngOnInit(): void {
   }
 
   criarPensamento() {
-    this.service.criar(this.pensamento).subscribe()
+    this.service.criar(this.pensamento).subscribe(() => {
+      this.router.navigate(['/listarPensamento'])
+    })
   }
 
   cancelar() {
-
+    this.router.navigate(['/listarPensamento'])
   }
 
 }
