8485e3aae0ac7d8a2e6197e49870bd6ef21ee4b4
Leonardo Andrade <154286969+LSAndradee@users.noreply.github.com>
2024-01-09 14:17:30 -0300

    Add files via upload

--- src/app/componentes/pensamentos/pensamento/pensamento.component.ts
+++ src/app/componentes/pensamentos/pensamento/pensamento.component.ts
@@ -13,7 +13,8 @@ export class PensamentoComponent implements OnInit {
     id: 0,
     conteudo: 'I love Angular',
     autoria: 'Leo',
-    modelo: 'modelo3'
+    modelo: 'modelo3',
+    favorito: false
   }
 
   constructor() { }
@@ -26,6 +27,13 @@ export class PensamentoComponent implements OnInit {
       return 'pensamento-g'
     }
     return 'pensamento-p'
+  }
+
+  mudarIconeFavorito(): string {
+    if(this.pensamento.favorito == false) {
+      return 'inativo'
+    }
+    return 'ativo'
   }
 
 }
